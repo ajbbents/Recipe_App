fe22118bbe2329ffba7db22880be783a73306d67
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-06-23 13:13:36 -0500

    get images up and running in db and on welcome pg

--- src/recipes/models.py
+++ src/recipes/models.py
@@ -3,9 +3,11 @@ from django.db import models
 
 class Recipe(models.Model):
     name = models.CharField(max_length=120)
+    description = models.TextField(default='this will be delicious!')
     cooking_time = models.FloatField(help_text="in minutes")
     difficulty = models.CharField(max_length=20)
     ingredients = models.TextField()
+    pic = models.ImageField(upload_to='recipes', default='no_picture.jpg')
 
     def __str__(self):
         return str(self.name)
