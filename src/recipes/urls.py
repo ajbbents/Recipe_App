89e0df2ca5d6461a865e346c2f66df6b15da3119
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-06-23 15:54:10 -0500

    add recipe grid and details functionality

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import recipes_home
+from .views import RecipeListView
+from .views import RecipeDetailView
 
 app_name = 'recipes'
 
 urlpatterns = [
     path('', recipes_home),
+    path('list/', RecipeListView.as_view(), name='list'),
+    path('list/<pk>', RecipeDetailView.as_view(), name='detail'),
 ]
--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render  # default import
+from django.views.generic import ListView, DetailView  # to display lists, details
+from .models import Recipe  # to access Recipe model
 
 # Create your views here.
+
+
+class RecipeListView(ListView):
+    model = Recipe
+    template_name = 'recipes/main.html'
+
+
+class RecipeDetailView(DetailView):
+    model = Recipe
+    template_name = 'recipes/detail.html'
 
 
 def recipes_home(request):
