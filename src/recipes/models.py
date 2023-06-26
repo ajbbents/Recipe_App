3d7ebe9c88459cb6efb26cd899cc0f3663e6d563
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-06-26 14:35:30 -0500

    finalize detail template

--- src/recipes/models.py
+++ src/recipes/models.py
@@ -6,9 +6,20 @@ class Recipe(models.Model):
     name = models.CharField(max_length=120)
     description = models.TextField(default='this will be delicious!')
     cooking_time = models.FloatField(help_text="in minutes")
-    difficulty = models.CharField(max_length=20)
     ingredients = models.TextField()
     pic = models.ImageField(upload_to='recipes', default='no_picture.jpg')
+
+    def calculate_difficulty(self):
+        ingredients = self.ingredients.split(', ')
+        if self.cooking_time < 10 and len(ingredients) < 7:
+            difficulty = 'Easy'
+        elif self.cooking_time < 10 and len(ingredients) >= 7:
+            difficulty = 'Medium'
+        elif self.cooking_time >= 10 and len(ingredients) < 7:
+            difficulty = 'Intermediate'
+        elif self.cooking_time >= 10 and len(ingredients) >= 7:
+            difficulty = 'Hard'
+        return difficulty
 
     def __str__(self):
         return str(self.name)
