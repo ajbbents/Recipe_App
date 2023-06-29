3e0ae282625ef28d054aa212b3bee3c7cb87f0b4
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-06-29 11:05:04 -0500

    institute authentication and login/out

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render  # default import
 from django.views.generic import ListView, DetailView  # to display lists, details
 from .models import Recipe  # to access Recipe model
+# to protect class-based view
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
 
-class RecipeListView(ListView):
+class RecipeListView(LoginRequiredMixin, ListView):
     model = Recipe
-    template_name = 'recipes/main.html'
+    template_name = 'recipes/recipes_list.html'
 
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipes/detail.html'
 
